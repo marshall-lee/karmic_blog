d49957ca77b3a3ec4f4a42fe4473f151d05ec70b
Vladimir Kochnev <hashtable@yandex.ru>
2015-02-15 13:50:36 +0300

    fix accessors

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,20 @@ class Post < ActiveRecord::Base
   validates_presence_of :author
   validates :rating, inclusion: { in: 0..5 }
 
-  attr_accessor :author_name
-  attr_accessor :category_name
+  attr_writer :author_name
+  def author_name
+    @author_name ||= if author then author.name end
+  end
+  
+  attr_writer :category_name
+  def category_name
+    @category_name ||= if category then category.name end
+  end
 
   before_validation(on: [:create, :update]) do |post|
     post.rating ||= rand(0..5)
-    post.author = Author.find_or_create_unique_by(name: author_name) if author_name
-    post.category = Category.find_or_create_unique_by(name: category_name) if category_name
+    post.author = Author.find_or_create_unique_by(name: author_name) if author_name.present?
+    post.category = Category.find_or_create_unique_by(name: category_name) if category_name.present?
   end
 
   after_create do |post|
